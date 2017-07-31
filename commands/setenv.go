7deb9ec2785385b9c0322677fd3cf6f5c8ceb763
Nicolas Chapon <nicolas.chapon@cnp.fr>
2017-07-31 17:42:56 +0200

    Initial Commit

---
+++ commands/setenv.go
@@ -0,0 +1,10 @@
+package commands
+
+import (
+	"fmt"
+)
+
+// setenv ...
+func Setenv(env string) {
+	fmt.Println("Set env", env)
+}
---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"kat/commands"
+	"os"
+
+	"github.com/urfave/cli"
+)
+
+// main ...
+func main() {
+
+	app := cli.NewApp()
+	app.Name = "KAT"
+	app.Description = "Kat Admin Tools"
+
+	app.Commands = []cli.Command{
+		{
+			Name:    "add-consumer",
+			Aliases: []string{"ac"},
+			Usage:   "Add a new CONSUMER",
+			Action: func(c *cli.Context) error {
+				fmt.Println("added consumer : ", c.Args().First())
+				return nil
+			},
+		},
+		{
+			Name:    "deploy",
+			Aliases: []string{"d"},
+			Usage:   "Deploy a new API",
+			Action: func(c *cli.Context) error {
+				fmt.Println("Deploying API", c.Args().First())
+				return nil
+			},
+		},
+		{
+			Name:    "setenv",
+			Aliases: []string{"s"},
+			Usage:   "Set up kong env",
+			Action: func(c *cli.Context) error {
+				fmt.Println("Setup env", c.Args().First())
+				commands.Setenv(c.Args().First())
+				return nil
+			},
+		},
+	}
+
+	app.CommandNotFound = func(c *cli.Context, command string) {
+		fmt.Fprintf(c.App.Writer, "Unknown command  %q here.\n", command)
+	}
+
+	app.Run(os.Args)
+}
